ec7f499d3dfa9eba5672fb7a49bbdf4988cc92c9
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-06-10 11:39:07 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,25 @@
-fun main(){
-    greetperson( "Hariet")
-    division(14, 34)
-    sum( 44, 40, 13 , 33)
-myself("I am a very curious persion")
+import kotlin.math.max
+
+fun main() {
+    greetperson("Hariet")
+    modulus(14, 34)
+    sum(44, 40, 13, 33)
+    myself("I am a very curious person")
 
 }
 
 fun greetperson(name: String){
-    println("Hello")
+    println("Hello $name")
 }
-fun division( num1: Int, num2: Int): Int{
-    var results  = num1. rem(num2)
-    println("$num1 / $num2 : results" )
-    return results
+fun modulus( num1: Int, num2: Int): Int.Companion {
+    var rem  = num1. rem(num2)
+    println("$num1 / $num2 modulus " )
+    return Int
 }
 fun sum(num1: Int , num2: Int , num3: Int ,num4: Int ): Int{
-    var sum = num1+num2+num3+num4
-    println(sum)
-    return sum
+    var x= num1+num2+num3+num4
+    println(x)
+    return x
 }
 fun myself(sentence: String){
     println("$sentence")
